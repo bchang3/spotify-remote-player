482b50fc5946a789295d7c85dcf00aea0fa5725c
Benjamin Chang <bchang@illinois.edu>
2025-01-09 13:19:54 -0600

    play red playlist when post is sent to endpoint

--- server/server.py
+++ server/server.py
@@ -2,9 +2,31 @@ from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
 from dotenv import load_dotenv
 import os
-
+import spotipy
+from spotify_player_utils import clearQueue, get_playlists
 
 load_dotenv()
+
+#set up spotify API client
+
+username = "jwpzy3f95b860qvipvqhxmira"
+clientID = os.getenv('SPOTIFY_CLIENT_ID',)
+clientSecret = os.getenv('SPOTIFY_CLIENT_SECRET',)
+redirect_uri = "http://google.com/callback/"
+scope = "user-read-playback-state playlist-modify-public user-modify-playback-state streaming app-remote-control playlist-read-collaborative playlist-read-private"
+oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri, scope=scope)
+token = oauth_object.get_access_token()['access_token']
+spotifyObject = spotipy.Spotify(token)
+
+user_name = spotifyObject.current_user()
+
+print(spotifyObject.devices()['devices'] )
+device = [x for x in spotifyObject.devices()['devices'] if x['name'] == "Benjamin’s MacBook Pro"][0] #gets the ID for specified device
+deviceID = device["id"]
+rgb_playlist = get_playlists(spotifyObject, deviceID) # a dictionary of the playlists called "red", "green", "blue" that the user has
+
+print(f"WELCOME TO THE PROJECT, {user_name['display_name']}\nCurrent Device: {device['name']}"  )
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback_default_key')
@@ -24,15 +46,27 @@ def handle_post_request():
         return jsonify({"error": "No JSON data provided"}), 400
     
     # Process the data (for example, log it or store it)
-    print(f"Received data: {data}")
-    
-    # Create a response
-    response = {
-        "message": "Data received successfully",
-        "received_data": data
-    }
-    
-    return jsonify(response), 200
+    print(f"Received message: {data}")
+    if (data.get("action") == "play_music"):
+        spotifyObject.pause_playback()
+        clearQueue(spotifyObject, deviceID)
+        playlist = rgb_playlist["red"]
+        for song in playlist:
+            spotifyObject.add_to_queue(song, deviceID)
+        spotifyObject.next_track(deviceID)
+
+        response = {
+          "message": "Starting playlist!",
+          "received_data": data
+        }
+        return jsonify(response), 200
+    else:
+      response = {
+          "message": "Action not found!",
+          "received_data": data
+      }
+      
+      return jsonify(response), 400
 
 if __name__ == '__main__':
     # Run the Flask app on port 5000
---
+++ server/spotify_player_utils.py
@@ -0,0 +1,26 @@
+import time
+
+def clearQueue(spotifyObject, deviceID):
+    print(len(spotifyObject.queue()))
+    for i in range(len(spotifyObject.queue())):
+        spotifyObject.next_track(deviceID)
+        time.sleep(0.1)
+    # spotifyObject.pause_playback(deviceID)
+    print("Queue cleared!")
+def get_playlists(spotifyObject, deviceID):
+    playlists = spotifyObject.current_user_playlists()
+    dict_playlists = {"red": [], "green": [], "blue": []}
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            if (playlist["name"] in ["red", "green", "blue"]):
+                playlistTracks = spotifyObject.playlist_tracks(playlist['uri'])
+                trackItems = playlistTracks['items']
+                for k in range(len(trackItems)):
+                    trackItem = trackItems[k]
+                    track = trackItem['track']
+                    dict_playlists[playlist["name"]].append(track['uri'])
+        if playlists['next']:
+            playlists = spotifyObject.next(playlists)
+        else:
+            playlists = None
+    return dict_playlists
